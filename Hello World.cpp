c9df418f15799cfdee6bc1d085aa0197d6cbb2d5
OmegaPrecursor <cyberfluxing@gmail.com>
2013-08-24 16:50:58 +0100

    Created Hello World.cpp
    This project is a welcome mat for me. To introduce myself onto this world.

---
+++ Hello World.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+cout<<"Hello World!\n This is OmegaPrecursor here. I will contribute to Open Source Projects from now";
+cin.get();
+return 0;
+}
